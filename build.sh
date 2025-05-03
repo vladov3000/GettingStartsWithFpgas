c46cd2a1f308ed3d308ef02d7b1736c60c51ced3
vladov3000 <vladov3000@gmail.com>
2025-05-03 09:03:12 -0700

    Merge build scripts.

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+set -e
+
+for argument in "$@"; do
+    case "$argument" in
+        --program)
+            program=true
+            ;;
+        *)
+            input=${argument%.vhdl}
+            ;;
+    esac
+done
+
+frequency=25 # MHz
+
+mkdir -p output
+ghdl analyze --workdir=output "$input.vhdl"
+(cd output && yosys -m ghdl -p "ghdl $input; synth_ice40 -json $input.json" -Q)
+nextpnr-ice40 --hx1k --package vq100 --pcf nandland_go_board.pcf --asc "output/$input.asc" --json output/"$input.json" --freq "$frequency"
+icetime -d hx1k -P vq100 -p nandland_go_board.pcf -r "output/$input.rpt" -t "output/$input.asc" -c "$frequency"
+icepack "output/$input.asc" output/"$input.bin"
+
+if [ -n "$program" ]; then
+    iceprog "output/$input.bin"
+fi
--- check.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-ghdl analyze --workdir=output "$1"
