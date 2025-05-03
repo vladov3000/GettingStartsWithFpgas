53b56ba738113e2ab0ed42811e1d33dbb7ec91c5
vladov3000 <vladov3000@gmail.com>
2025-05-03 08:41:50 -0700

    Check timing constraints.

--- program.sh
+++ program.sh
@@ -7,11 +7,13 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-export NAME="${1%.vhdl}"
+NAME="${1%.vhdl}"
+CLOCK_FREQUENCY=25 # MHz
 
 mkdir -p output
 ghdl analyze --workdir=output "$NAME.vhdl"
 (cd output && yosys -m ghdl -p "ghdl $NAME; synth_ice40 -json $NAME.json" -Q)
-nextpnr-ice40 --hx1k --package vq100 --pcf nandland_go_board.pcf --asc output/"$NAME.asc" --json output/"$NAME.json"
-icepack output/"$NAME.asc" output/"$NAME.bin"
-iceprog output/"$NAME.bin"
+nextpnr-ice40 --hx1k --package vq100 --pcf nandland_go_board.pcf --asc "output/$NAME.asc" --json output/"$NAME.json" --freq "$CLOCK_FREQUENCY"
+icetime -d hx1k -P vq100 -p nandland_go_board.pcf -r "output/$NAME.rpt" -t "output/$NAME.asc" -c "$CLOCK_FREQUENCY"
+icepack "output/$NAME.asc" output/"$NAME.bin"
+iceprog "output/$NAME.bin"
